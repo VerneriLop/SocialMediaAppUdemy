3c13519d5e81d4738b1dc060f8a6adf1a52087f5
Verneri <verneri.lopperi@gmail.com>
2024-05-15 11:56:31 +0300

    custom font installation

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
+import {getFontFamily} from './assets/fonts/helper';
 
 const App = () => {
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView>
+      <Text style={{fontSize: 50, fontFamily: getFontFamily('Inter', '500')}}>
+        Hello world!
+      </Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
