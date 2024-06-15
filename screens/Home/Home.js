0c36704f167e6fb525d831f7c73aeb311272db83
Verneri <verneri.lopperi@gmail.com>
2024-06-15 12:12:23 +0300

    using useMemo to get rid off useEffect dependency warning

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import {
   FlatList,
   SafeAreaView,
@@ -20,111 +20,117 @@ import {scaleFontSize} from '../../assets/styles/scaling';
 import globalStyle from '../../assets/styles/globalStyle';
 
 const Home = () => {
-  const userStories = [
-    {
-      firstName: 'Joseph',
-      id: 1,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Angel',
-      id: 2,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'White',
-      id: 3,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Olivier',
-      id: 4,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Nata',
-      id: 5,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Nicolas',
-      id: 6,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Nino',
-      id: 7,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Nana',
-      id: 8,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-    {
-      firstName: 'Verneri',
-      id: 9,
-      profileImage: require('../../assets/images/default_profile.png'),
-    },
-  ];
+  const userStories = useMemo(
+    () => [
+      {
+        firstName: 'Joseph',
+        id: 1,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Angel',
+        id: 2,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'White',
+        id: 3,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Olivier',
+        id: 4,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Nata',
+        id: 5,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Nicolas',
+        id: 6,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Nino',
+        id: 7,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Nana',
+        id: 8,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+      {
+        firstName: 'Verneri',
+        id: 9,
+        profileImage: require('../../assets/images/default_profile.png'),
+      },
+    ],
+    [],
+  );
 
-  const userPosts = [
-    {
-      firstName: 'Verneri',
-      lastName: 'Lopperi',
-      location: 'Boston, MA',
-      likes: 1201,
-      comments: 24,
-      bookmarks: 55,
-      image: require('../../assets/images/default_post.png'),
-      profileImage: require('../../assets/images/default_profile.png'),
-      id: 1,
-    },
-    {
-      firstName: 'Jennifer',
-      lastName: 'Wilson',
-      location: 'Worcester, MA',
-      likes: 1301,
-      comments: 25,
-      bookmarks: 70,
-      image: require('../../assets/images/default_post.png'),
-      profileImage: require('../../assets/images/default_profile.png'),
-      id: 2,
-    },
-    {
-      firstName: 'Adam',
-      lastName: 'Ondra',
-      location: 'Worcester, MA',
-      likes: 1501,
-      comments: 225,
-      bookmarks: 90,
-      image: require('../../assets/images/default_post.png'),
-      profileImage: require('../../assets/images/default_profile.png'),
-      id: 3,
-    },
-    {
-      firstName: 'Nata',
-      lastName: 'Vates',
-      location: 'New York, NY',
-      likes: 120,
-      comments: 15,
-      bookmarks: 20,
-      image: require('../../assets/images/default_post.png'),
-      profileImage: require('../../assets/images/default_profile.png'),
-      id: 4,
-    },
-    {
-      firstName: 'Nicholas',
-      lastName: 'Namoradze',
-      location: 'Berlin, Germany',
-      likes: 2000,
-      comments: 32,
-      bookmarks: 22,
-      image: require('../../assets/images/default_post.png'),
-      profileImage: require('../../assets/images/default_profile.png'),
-      id: 5,
-    },
-  ];
+  const userPosts = useMemo(
+    () => [
+      {
+        firstName: 'Verneri',
+        lastName: 'Lopperi',
+        location: 'Boston, MA',
+        likes: 1201,
+        comments: 24,
+        bookmarks: 55,
+        image: require('../../assets/images/default_post.png'),
+        profileImage: require('../../assets/images/default_profile.png'),
+        id: 1,
+      },
+      {
+        firstName: 'Jennifer',
+        lastName: 'Wilson',
+        location: 'Worcester, MA',
+        likes: 1301,
+        comments: 25,
+        bookmarks: 70,
+        image: require('../../assets/images/default_post.png'),
+        profileImage: require('../../assets/images/default_profile.png'),
+        id: 2,
+      },
+      {
+        firstName: 'Adam',
+        lastName: 'Ondra',
+        location: 'Worcester, MA',
+        likes: 1501,
+        comments: 225,
+        bookmarks: 90,
+        image: require('../../assets/images/default_post.png'),
+        profileImage: require('../../assets/images/default_profile.png'),
+        id: 3,
+      },
+      {
+        firstName: 'Nata',
+        lastName: 'Vates',
+        location: 'New York, NY',
+        likes: 120,
+        comments: 15,
+        bookmarks: 20,
+        image: require('../../assets/images/default_post.png'),
+        profileImage: require('../../assets/images/default_profile.png'),
+        id: 4,
+      },
+      {
+        firstName: 'Nicholas',
+        lastName: 'Namoradze',
+        location: 'Berlin, Germany',
+        likes: 2000,
+        comments: 32,
+        bookmarks: 22,
+        image: require('../../assets/images/default_post.png'),
+        profileImage: require('../../assets/images/default_profile.png'),
+        id: 5,
+      },
+    ],
+    [],
+  );
 
   const userStoriesPageSize = 4;
   const [userStoriesCurrentPage, setUserStoriesCurrentPage] = useState(1);
@@ -167,7 +173,7 @@ const Home = () => {
       setScreenData(result.screen);
       console.log('RESULT', result);
     });*/
-  }, []);
+  }, [userPosts, userStories]);
 
   return (
     <SafeAreaView style={globalStyle.backgroundWhite}>
